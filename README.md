064bd10493f313b06a541058708311e0d0deb51d
tdallas <tdallas@itba.edu.ar>
2018-07-31 19:16:47 -0300

    Initial commit for inge-tpe

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# inge-tpe
-Repo para el TPE de Ingenieria de Software I
+# Inge Trabajo Práctico Especial
+Instrucciones de instalacion:
+1) Instalar maven:
+    sudo apt-get install maven
+    
+   TODO (Armar un script para buildear back y front)
+2) Compilar el backend
+3) Levantar el backend
---
+++ inge-backend/src/main/java/com/inge/inge/IngeApplication.java
@@ -0,0 +1,12 @@
+package com.inge.inge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngeApplication.class, args);
+	}
+}
