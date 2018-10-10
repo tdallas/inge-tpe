fc479efa9dca017ba44aece5bed37d8ca388132e
tdallas <tdallas@itba.edu.ar>
2018-10-10 16:11:05 -0300

    Cambie proyecto backend, saque paquetes innecesarios, falta configurar DB

---
+++ inge-backend/inge-app/src/main/java/com/inge/ingeapp/IngeAppApplication.java
@@ -0,0 +1,26 @@
+package com.inge.ingeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class IngeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngeAppApplication.class, args);
+	}
+}
+
+
+@RestController
+class GreetingController {
+
+	@RequestMapping("/hello/{name}")
+	String hello(@PathVariable String name) {
+		return "Hello, " + name + "!";
+	}
+}
