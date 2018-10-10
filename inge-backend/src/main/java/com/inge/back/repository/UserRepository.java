083e046e276bec79382cb9d4fd5e1cc9de4e74e6
tdallas <tdallas@itba.edu.ar>
2018-10-10 16:17:26 -0300

    Borro cosas del proyecto anterior

--- inge-backend/src/main/java/com/inge/back/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.inge.back.repository;
-
-import com.inge.back.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Usuario, Long> {
-}
