cca3fa5fd9e7f7646c7b3cf4c333b7d2702f5de0
tdallas <tdallas@itba.edu.ar>
2018-12-05 21:48:12 -0300

    arreglue margen de boton signup

--- inge-frontend/app/src/main/java/com/ingeapp/model/request/SignUpRequest.java
+++ inge-frontend/app/src/main/java/com/ingeapp/model/request/SignUpRequest.java
@@ -17,4 +17,52 @@ public class SignUpRequest {
         this.dni = dni;
         this.telefono = telefono;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
